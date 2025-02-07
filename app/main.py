06c462c027e1caa53050340b642d26575b068ce6
pat-rap <patrap9919@gmail.com>
2025-02-07 19:13:23 +0900

    If-Modified-Since ヘッダーを利用

--- app/main.py
+++ app/main.py
@@ -1,8 +1,18 @@
 from fastapi import FastAPI, Depends, Response, Request
-from auth import get_current_user, set_auth_cookie, remove_auth_cookie, TokenData
-from rss_reader import fetch_rss_feed, parse_rss_feed
+from .auth import get_current_user, set_auth_cookie, remove_auth_cookie, TokenData
+from .rss_reader import fetch_rss_feed, parse_rss_feed
+from datetime import datetime
 
 app = FastAPI()
+
+# last_modified をフィードタイプごとに保持する辞書
+last_modified_times = {
+    "regular": None,
+    "extra": None,
+    "eqvol": None,
+    "other": None
+}
+
 
 @app.get("/")
 async def root():
@@ -26,6 +36,7 @@ async def logout(response: Response):
 
 @app.get("/rss/{feed_type}")
 async def read_rss(feed_type: str):
+    global last_modified_times
     if feed_type == "regular":
         url = "https://www.data.jma.go.jp/developer/xml/feed/regular.xml"
     elif feed_type == "extra":
@@ -37,10 +48,19 @@ async def read_rss(feed_type: str):
     else:
         return {"error": "Invalid feed type"}
 
-    response = fetch_rss_feed(url)
+    response = fetch_rss_feed(url, last_modified_times[feed_type])
+    if response is None:  # 304 Not Modified
+        return {"message": "No update or error fetching feed"}
+
     if response:
         entries, feed_title, feed_subtitle, feed_updated, feed_id_in_atom, rights = parse_rss_feed(response)
+
+        # Last-Modified ヘッダーをパースして保存
+        last_modified_str = response.headers.get('Last-Modified')
+        if last_modified_str:
+            last_modified_times[feed_type] = datetime.strptime(last_modified_str, '%a, %d %b %Y %H:%M:%S %Z')
+
         return {"feed_title": feed_title, "entries": entries, "feed_subtitle":feed_subtitle, "feed_updated":feed_updated, "feed_id_in_atom":feed_id_in_atom, "rights":rights}
     else:
-        return {"message": "No update or error fetching feed"}
+        return {"message": "No update or error fetching feed"} #fetch_rss_feed内で例外が発生した場合
 
