3024573f2e17211de897b3a12b07e37f277c2c74
pat-rap <patrap9919@gmail.com>
2025-02-13 13:43:44 +0900

    utcnowをtimezoneへ変更

--- app/auth.py
+++ app/auth.py
@@ -1,10 +1,8 @@
-from fastapi import Depends, HTTPException, status, Request, Response
-from datetime import datetime, timedelta
+from fastapi import HTTPException, status, Request, Response
+from datetime import datetime, timedelta, timezone
 from typing import Optional
-
 from jose import JWTError, jwt
 from pydantic import BaseModel
-
 from dotenv import load_dotenv
 import os
 import logging
@@ -24,9 +22,9 @@ class TokenData(BaseModel):
 def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
     to_encode = data.copy()
     if expires_delta:
-        expire = datetime.utcnow() + expires_delta
+        expire = datetime.now(timezone.utc) + expires_delta
     else:
-        expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+        expire = datetime.now(timezone.utc) + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
     to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
